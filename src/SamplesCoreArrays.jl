253f23ea35accc274e90476abb89f42eecac3870
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-13 12:45:38 -0400

    Update SamplesCoreArrays.jl

--- src/SamplesCoreArrays.jl
+++ src/SamplesCoreArrays.jl
@@ -7,30 +7,59 @@ export RelativeOrigin, TypedOrigin, DomainOffsetTypes,
 
 struct RelativeOrigin end
 struct TypedOrigin{O} end
-const DomainOffsetTypes = Union{Number,RelativeOrigin,TypedOrigin}
 struct SampleSpace end
 struct TypedDomainSpace{D} end
+abstract type AbstractDomainArray{T,N} <: AbstractArray{T,N} end
+
+const DomainOffsetTypes = Union{Number,RelativeOrigin,TypedOrigin}
 const GenericDomainRateTypes = Union{Number,SampleSpace,TypedDomainSpace}
 const IntegerIndexTypes = Union{Integer,AbstractRange,CartesianIndex,AbstractArray{<:Integer}}
 # The fields are in sample space. (index, offset, even rate is in sample space for Domains to interpret!)
+
+extract_origin(::RelativeOrigin) = 0
+extract_origin(::TypedOrigin{O}) where {O} = O
+extract_origin(o::Number) = o
+rate_converter(rate::R) where {R} = rate # Allow arbitrary user-defined rate types (Unitful, Measurements, etc.)
+rate_converter(rate::SampleSpace) = 1
+rate_converter(::TypedDomainSpace{D}) where {D} = D
 struct DomainIndex{TIO<:DomainOffsetTypes,TIR,DomainOnlyI}
-    origin_shift::TIO
     index::Int
+    origin_shift::TIO
     rate::TIR
-    DomainIndex(origin_shift::TIO,index::Integer,rate::TIR) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,false}(origin_shift,index,rate)
-    DomainIndex(origin_shift::TIO,index::Integer,rate::TIR;DomainOnly::Bool) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,DomainOnly}(origin_shift,index,rate)
 end
+DomainIndex(index::Integer, origin_shift::TIO, rate::TIR) where {TIO<:DomainOffsetTypes,TIR} = DomainIndex{TIO,TIR,false}(index, origin_shift, rate)
+DomainIndex(index::Integer, origin_shift::TIO) where {TIO<:DomainOffsetTypes} = DomainIndex(index, origin_shift, SampleSpace())
+DomainIndex(index::Integer) = DomainIndex(index, RelativeOrigin())
+DomainOnlyIndex(index::Integer, origin_shift::TIO, rate::TIR) where {TIO<:DomainOffsetTypes,TIR} = DomainIndex{TIO,TIR,true}(index, origin_shift, rate)
+DomainOnlyIndex(index::Integer, origin_shift::TIO) where {TIO<:DomainOffsetTypes} = DomainOnlyIndex(index, origin_shift, SampleSpace())
+DomainOnlyIndex(index::Integer) = DomainOnlyIndex(index, RelativeOrigin())
+
 struct DomainAxis{TAO<:DomainOffsetTypes,TAR,DomainOnlyA}
     origin_shift::TAO
     rate::TAR
-    DomainAxis(origin_shift::TAO,rate::TAR) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,false}(origin_shift,rate)
-    DomainAxis(origin_shift::TAO,rate::TAR;DomainOnly::Bool) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,DomainOnly}(origin_shift,rate)
 end
-abstract type AbstractDomainArray{T,N} <: AbstractArray{T,N} end
+DomainAxis(origin_shift::TAO,rate::TAR) where {TAO<:DomainOffsetTypes,TAR} = DomainAxis{TAO,TAR,false}(origin_shift,rate)
+DomainAxis(origin_shift::TAO) where {TAO<:DomainOffsetTypes} = DomainAxis(origin_shift,SampleSpace())
+DomainAxis() = DomainAxis(RelativeOrigin())
+DomainOnlyAxis(origin_shift::TAO,rate::TAR) where {TAO<:DomainOffsetTypes,TAR} = DomainAxis{TAO,TAR,true}(origin_shift,rate)
+DomainOnlyAxis(origin_shift::TAO) where {TAO<:DomainOffsetTypes} = DomainOnlyAxis(origin_shift,SampleSpace())
+DomainOnlyAxis() = DomainOnlyAxis(RelativeOrigin())
+DomainAxisFromDomain(::RelativeOrigin,rate::TAR) where {TAR} = DomainAxis(RelativeOrigin(),rate)
+DomainAxisFromDomain(::TypedOrigin{O},rate::TAR) where {O,TAR} = DomainAxis(TypedOrigin{O*rate_converter(rate)}(),rate)
+DomainAxisFromDomain(origin_shift::T,rate::TAR) where {T<:Number} = DomainAxis(T(origin_shift*rate_converter(rate)),rate)
+DomainAxisFromDomain(origin_shift::TAO) where {TAO<:DomainOffsetTypes} = DomainAxisFromDomain(origin_shift,SampleSpace())
+DomainAxisFromDomain() = DomainAxis(RelativeOrigin())
+DomainOnlyAxisFromDomain(::RelativeOrigin,rate::TAR) where {TAR} = DomainOnlyAxis(RelativeOrigin(),rate)
+DomainOnlyAxisFromDomain(::TypedOrigin{O},rate::TAR) where {O,TAR} = DomainOnlyAxis(TypedOrigin{O*rate_converter(rate)}(),rate)
+DomainOnlyAxisFromDomain(origin_shift::T,rate::TAR) where {T<:Number} = DomainOnlyAxis(T(origin_shift*rate_converter(rate)),rate)
+DomainOnlyAxisFromDomain(origin_shift::TAO) where {TAO<:DomainOffsetTypes} = DomainOnlyAxisFromDomain(origin_shift,SampleSpace())
+DomainOnlyAxisFromDomain() = DomainOnlyAxis(RelativeOrigin())
+
 struct DomainArray{T,N,A<:AbstractArray{T,N},Dims<:NTuple{N,DomainAxis}} <: AbstractDomainArray{T,N}
     data::A
     dims::Dims
 end
+DomainArray(data::AbstractArray{T,N}) where {T,N} = DomainArray(data, ntuple(_ -> DomainAxis(), N))
 Base.size(D::DomainArray) = size(D.data)
 Base.axes(D::DomainArray) = axes(D.data)
 @inline function check_index_allowed(axis::DomainAxis{TAO,TAR,DomainOnlyA},idx::DomainIndex{TIO,TIR,DomainOnlyI}) where {TAO,TAR,TIO,TIR,DomainOnlyA,DomainOnlyI}
@@ -43,7 +72,7 @@ Base.axes(D::DomainArray) = axes(D.data)
     return true
 end
 @inline check_index_allowed(axis::DomainAxis{TAO,TAR,false}, idx::T) where {TAO,TAR,T<:IntegerIndexTypes} = true
-@inline check_index_allowed(axis::DomainAxis{TAO,TAR,true}, idx::T) where {TAO,TAR,T<:IntegerIndexTypes} =error("Index type $T forbidden on this axis (DomainOnly=true). Use DomainIndex instead.")
+@inline check_index_allowed(axis::DomainAxis{TAO,TAR,true}, idx::T) where {TAO,TAR,T<:IntegerIndexTypes} = error("Index type $T forbidden on this axis (DomainOnly=true). Use DomainIndex instead.")
 @inline function convert_index(axis::DomainAxis, idx::DomainIndex)
     check_index_allowed(axis, idx)
     return convert_domain_index(axis, idx)
@@ -61,12 +90,6 @@ function Base.getindex(D::DomainArray, I...)
     return getindex(D.data, converted...)
 end
 
-extract_origin(::RelativeOrigin) = 0
-extract_origin(o::TypedOrigin{O}) where {O} = O
-extract_origin(o::Number) = o
-rate_converter(rate::R) = rate # Allow arbitrary user-defined rate types (Unitful, Measurements, etc.)
-rate_converter(rate::SampleSpace) = 1
-rate_converter(rate::TypedDomainSpace{D}) = D
 function convert_domain_index(axis::DomainAxis{TAO,TAR,DomainOnlyA},i::DomainIndex{TIO,TIR,DomainOnlyI}) where {TAO<:DomainOffsetTypes,TAR<:GenericDomainRateTypes,TIO<:DomainOffsetTypes,TIR<:GenericDomainRateTypes,DomainOnlyA,DomainOnlyI}
     axis_rate, index_rate = rate_converter(axis.rate), rate_converter(i.rate)
     origin_axis, origin_index = extract_origin(axis.origin_shift), extract_origin(i.origin_shift)
@@ -88,6 +111,31 @@ function convert_domain_index(axis::DomainAxis{TAO,TAR,DomainOnlyA},i::DomainInd
     end
 end
 
+normalize_sample_dim(n::Integer) = (n, DomainAxis())
+normalize_sample_dim(t::Tuple{<:Integer,DomainAxis}) = (t[1], t[2])
+normalize_sample_dim(t::Tuple{<:Integer,Vararg}) = (t[1], DomainAxis(t[2:end]...))
+normalize_sample_dim_from_domain(n::T) where {T} = (ceil(Int,n), DomainAxisFromDomain())
+normalize_sample_dim_from_domain(t::Tuple{T,DomainAxis}) where {T} = (ceil(Int,t[1]*rate_converter(t[2].rate)),t[2])
+normalize_sample_dim_from_domain(t::Tuple{T,Vararg}) where {T} = normalize_sample_dim_from_domain(t[1],DomainAxisFromDomain(t[2:end]...))
+normalize_sample_dims(dims...) = map(normalize_sample_dim, dims)
+normalize_sample_dims_from_domain(dims...) = map(normalize_sample_dim_from_domain, dims)
+
+function DomainZeros(::Type{T}, dims_argument) where {T}
+    dims = normalize_sample_dims(dims_argument...)
+    sizes = map(first, dims)
+    axes  = map(last, dims)
+    data  = zeros(T, Tuple(sizes))
+    return DomainArray(data, Tuple(axes))
+end
+
+function DomainZerosFromDomain(::Type{T}, dims_argument) where {T}
+    dims = normalize_sample_dims_from_domain(dims_argument...)
+    sizes = map(first, dims)
+    axes  = map(last, dims)
+    data  = zeros(T, Tuple(sizes))
+    return DomainArray(data, Tuple(axes))
+end
+
 
 ############################
 # domainaxes
@@ -100,18 +148,6 @@ function domainaxes(D::DomainArray{T,N,A,Dims,DomainOnly}, dim::Int) where {T,N,
     offset = diminfo.offset
     return (DomainIndex{rate, typeof(offset), DomainOnly}(i, offset) for i in 1:len)
 end
-
-############################
-# Strict domain mapping
-############################
-
-offset_to_number(o::RelativeOrigin) = 0
-offset_to_number(o::TypedOrigin{O}) where {O} = O
-offset_to_number(o) = o  # numeric
-
-rate_to_number(r::SampleSpace) = 1
-rate_to_number(r::TypedDomainSpace{D}) where {D} = D
-rate_to_number(r) = r  # numeric / unitful
 
 function _broadcast_safe(D::DomainArray)
     for ax in D.dims
@@ -129,63 +165,6 @@ function broadcasted(f, D::DomainArray, A::AbstractArray)
         error("Cannot broadcast DomainArray with DomainOnly axes against a normal array.")
     end
     return DomainArray(broadcast(f, D.data, A), D.dims)
-end
-
-"""
-    domainmap(D, dim, idx)
-
-Strictly map a DomainIndex `idx` on dimension `dim` to a sample index in `D`.
-"""
-function domainmap(D::DomainArray, dim::Int,
-                   idx::DomainIndex{Ri,TIO,DomainOnly}) where {Ri,TIO,DomainOnly}
-
-    diminfo = D.dims[dim]
-
-    # Source (index)
-    Ri_num = rate_to_number(Ri)
-    io     = offset_to_number(idx.offset)
-
-    # Destination (array)
-    Ra_num = rate_to_number(diminfo.rate)
-    ao     = offset_to_number(diminfo.offset)
-
-    i = idx.index
-
-    ###############################################
-    # Case 1: Same rate
-    ###############################################
-    if Ri_num == Ra_num
-        delta = io - ao
-        if isinteger(delta)
-            return i + Int(delta)
-        else
-            error("DomainIndex does not align with destination sampling grid")
-        end
-    end
-
-    ###############################################
-    # Case 2: Integer/integer rational alignment
-    ###############################################
-    if Ri_num isa Integer && Ra_num isa Integer
-        num = i * Ra_num + (io - ao)
-        if num % Ri_num == 0
-            return div(num, Ri_num)
-        else
-            error("DomainIndex does not align with destination sampling grid")
-        end
-    end
-
-    ###############################################
-    # Case 3: General real-valued rates
-    ###############################################
-    t = (i - 1 + io) / Ri_num
-    j_real = t * Ra_num - ao + 1
-
-    if isinteger(j_real)
-        return Int(j_real)
-    else
-        error("DomainIndex does not map exactly to a sample index")
-    end
 end
 
 end # module
