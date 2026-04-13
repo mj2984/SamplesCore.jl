7f5db52a274f207c9040796649e5d696557a6452
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-13 09:06:38 -0400

    Update SamplesCoreArrays.jl

--- src/SamplesCoreArrays.jl
+++ src/SamplesCoreArrays.jl
@@ -12,80 +12,80 @@ struct SampleSpace end
 struct TypedDomainSpace{D} end
 # The fields are in sample space. (index, offset, even rate is in sample space for Domains to interpret!)
 struct DomainIndex{TIO<:DomainOffsetTypes,TIR,DomainOnlyI}
-    offset::TIO
+    origin_shift::TIO
     index::Int
     rate::TIR
-    DomainIndex(offset::TIO,index::Integer,rate::TIR) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,false}(offset,index,rate)
-    DomainIndex(offset::TIO,index::Integer,rate::TIR;DomainOnly::Bool) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,DomainOnly}(offset,index,rate)
+    DomainIndex(origin_shift::TIO,index::Integer,rate::TIR) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,false}(origin_shift,index,rate)
+    DomainIndex(origin_shift::TIO,index::Integer,rate::TIR;DomainOnly::Bool) where {TIO<:DomainOffsetTypes,TIR} = new{TIO,TIR,DomainOnly}(origin_shift,index,rate)
 end
 struct DomainAxis{TAO<:DomainOffsetTypes,TAR,DomainOnlyA}
-    offset::TAO
+    origin_shift::TAO
     rate::TAR
-    DomainAxis(offset::TAO,rate::TAR) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,false}(offset,rate)
-    DomainAxis(offset::TAO,rate::TAR;DomainOnly::Bool) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,DomainOnly}(offset,rate)
+    DomainAxis(origin_shift::TAO,rate::TAR) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,false}(origin_shift,rate)
+    DomainAxis(origin_shift::TAO,rate::TAR;DomainOnly::Bool) where {TAO<:DomainOffsetTypes,TAR} = new{TAO,TAR,DomainOnly}(origin_shift,rate)
 end
-struct DomainArray{T,N,A<:AbstractArray{T,N},Dims<:NTuple{N,DomainAxis}} <: AbstractArray{T,N}
+abstract type AbstractDomainArray{T,N} <: AbstractArray{T,N} end
+struct DomainArray{T,N,A<:AbstractArray{T,N},Dims<:NTuple{N,DomainAxis}} <: AbstractDomainArray{T,N}
     data::A
     dims::Dims
 end
 Base.size(D::DomainArray) = size(D.data)
 Base.axes(D::DomainArray) = axes(D.data)
-# Normal indexing with integers: allowed only on axes where DomainOnlyA == false
+function check_index_allowed(axis::DomainAxis{TAO,TAR,DomainOnlyA},idx::DomainIndex{TIO,TIR,DomainOnlyI}) where {TAO,TAR,TIO,TIR,DomainOnlyA,DomainOnlyI}
+    if axis.rate isa SampleSpace && DomainOnlyI
+        error("Cannot use a domain-only index on a SampleSpace axis")
+    end
+    if idx.rate isa SampleSpace && DomainOnlyA
+        error("Cannot use a SampleSpace index on a domain-only axis")
+    end
+    return true
+end
+check_index_allowed(axis::DomainAxis,::Colon) = true
+check_index_allowed(axis::DomainAxis{TAO,TAR,false}, idx::T) where {TAO,TAR,T<:Union{Integer,AbstractRange,CartesianIndex,AbstractArray{<:Integer}}} = true
+check_index_allowed(axis::DomainAxis{TAO,TAR,true},idx::T) where {TAO,TAR,T<:Union{Integer,AbstractRange,CartesianIndex,AbstractArray{<:Integer}}} =error("Index type $T forbidden on this axis (DomainOnly=true). Use DomainIndex instead.")
+function convert_index(axis::DomainAxis, idx)
+    check_index_allowed(axis, idx)
+    if idx isa DomainIndex
+        return NoInterpolationDomainIndexer(axis, idx)
+    elseif idx isa Colon
+        return Colon()
+    else
+        return idx
+    end
+end
 function Base.getindex(D::DomainArray, I...)
-    # Check dimensionality
     length(I) == length(D.dims) ||
         throw(DimensionMismatch("Expected $(length(D.dims)) indices, got $(length(I))"))
 
-    OutputIndices = ()
+    converted = ntuple(k -> convert_index(D.dims[k], I[k]), length(I))
+    return getindex(D.data, converted...)
+end
 
-    for (k, idx) in enumerate(I)
-        axis = D.dims[k]
-
-        if idx isa Integer
-            # Integer indexing allowed only if DomainOnlyA == false
-            if axis.DomainOnlyA
-                error("Normal indexing forbidden on axis $k (DomainOnly=true). Use DomainIndex instead.")
-            end
-            OutputIndices = (OutputIndices..., idx)
-
-        elseif idx isa DomainIndex
-            # Convert DomainIndex → sample index
-            OutputIndices = (OutputIndices..., NoInterpolationDomainIndexer(axis, idx))
-
-        elseif idx === Colon()
-            # Colon indexing always allowed
-            OutputIndices = (OutputIndices..., Colon())
-
-        elseif idx isa AbstractRange
-            # Range indexing allowed only if axis is not DomainOnly
-            if axis.DomainOnlyA
-                error("Range indexing forbidden on axis $k (DomainOnly=true). Use DomainIndex instead.")
-            end
-            OutputIndices = (OutputIndices..., idx)
-
+extract_origin(::RelativeOrigin) = 0
+extract_origin(o::TypedOrigin{O}) where {O} = O
+extract_origin(o::Number) = o
+rate_converter(rate::R) = rate # Allow arbitrary user-defined rate types (Unitful, Measurements, etc.)
+rate_converter(rate::SampleSpace) = 1
+rate_converter(rate::TypedDomainSpace{D}) = D
+function NoInterpolationDomainIndexer(axis::DomainAxis{TAO,TAR,DomainOnlyA},i::DomainIndex{TIO,TIR,DomainOnlyI}) where {TAO,TAR,TIO,TIR,DomainOnlyA,DomainOnlyI}
+    axis_rate, index_rate = rate_converter(axis.rate), rate_converter(i.rate)
+    origin_axis, origin_index = extract_origin(axis.origin_shift), extract_origin(i.origin_shift)
+    index = i.index
+    if index_rate == axis_rate
+        delta = origin_index - origin_axis
+        if isinteger(delta)
+            return index + Int(delta)
         else
-            error("Unsupported index type $(typeof(idx)) for axis $k")
+            error("DomainIndex offset does not align with axis offset")
+        end
+    else
+        axis_index = ((index + origin_index - 1) * axis_rate / index_rate) - origin_axis + 1
+        if isinteger(axis_index)
+            return Int(axis_index)
+        else
+            error("DomainIndex does not map exactly to a sample index")
         end
     end
-
-    return getindex(D.data, OutputIndices...)
-end
-
-# DomainIndex indexing
-function Base.getindex(D::DomainArray{T,N,A,Dims}, i::DomainIndex{TIO,TIR,DomainOnlyI}, I...) where {T,N,A,Dims,Rate,TIO,DomainOnly}
-    iD = NoInterpolationDomainIndexer(D, 1, i)   # strict mapping along dimension 1
-    return getindex(D.data, iD, I...)
-end
-
-extract_offset(offset::TypedOrigin{O}) = O
-extract_offset(offset::Number) = offset
-extract_offset(offset::RelativeOrigin) = 0
-extract_rate(rate::TypedDomainSpace{D}) = D
-extract_rate(rate::Number) = rate
-#extract_rate(rate::SampleSpace) = 1
-function isRateMatched(indexRate::TIR,axisRate::TAR,DomainOnlyI::Bool,DomainOnlyR::Bool)
-function NoInterpolationDomainIndexer(i::DomainIndex{TIO,TIR,DomainOnlyI},a::DomainDimension{TAO,TAR,DomainOnlyR})
-
 end
 
 
@@ -112,6 +112,24 @@ offset_to_number(o) = o  # numeric
 rate_to_number(r::SampleSpace) = 1
 rate_to_number(r::TypedDomainSpace{D}) where {D} = D
 rate_to_number(r) = r  # numeric / unitful
+
+function _broadcast_safe(D::DomainArray)
+    for ax in D.dims
+        if ax.DomainOnlyA
+            return false
+        end
+    end
+    return true
+end
+
+import Base.Broadcast: broadcasted, materialize
+
+function broadcasted(f, D::DomainArray, A::AbstractArray)
+    if !_broadcast_safe(D)
+        error("Cannot broadcast DomainArray with DomainOnly axes against a normal array.")
+    end
+    return DomainArray(broadcast(f, D.data, A), D.dims)
+end
 
 """
     domainmap(D, dim, idx)
